44a84b2a647005df5d8b64bcf7b5c8136928cf3a
Onur Elhakan <72891423+oelhakan@users.noreply.github.com>
2020-12-28 12:56:16 +0100

    Update TeenNumberChecker.java

--- TeenNumberChecker.java
+++ TeenNumberChecker.java
@@ -1,27 +1,27 @@
 public class TeenNumberChecker {    //WE WILL SAY A NUMBE IS TEEN IF ITS IN THE RANGE 13-19|(INCLUSIVE)
 
-    public static boolean hasTeen(int number1, int number2, int number3) {
-        //IF ANY OF THE PARAMETERS ARE IN THE RANGE OF 13-19 RETURN TRUE(13 AND 19 INCLUSIVE)
-        if (((13 <= number1) && (19 >= number1)) || ((13 <= number2) && (19 >= number2)) || ((13 <= number3) && (19 >= number3))) {
-            return true;
-        } else return false;
-    }
+public static boolean hasTeen(int number1, int number2, int number3) {
+            //IF ANY OF THE PARAMETERS ARE IN THE RANGE OF 13-19 RETURN TRUE(13 AND 19 INCLUSIVE)
+            if ( isTeen(number1) || isTeen(number2) || isTeen(number3)) {
+                return true;
+            } else return false;
+        }
 
-    public static boolean isTeen(int number1) {
-        //IF THE PARAMETER IS IN THE RANGE OF 13-19 RETURN TRUE(13 AND 19 INCLUSIVE)
-        if ((13 <= number1) && (19 >= number1)) {
-            return true;
-        } else return false;
-    }
+        public static boolean isTeen(int number1) {
+            //IF THE PARAMETER IS IN THE RANGE OF 13-19 RETURN TRUE(13 AND 19 INCLUSIVE)
+            if ((13 <= number1) && (19 >= number1)) {
+                return true;
+            } else return false;
+        }
 
-    public static void main(String[] args) {
-        System.out.println(hasTeen(1, 2, 3));
-        System.out.println(hasTeen(13, 2, 3));
-        System.out.println(hasTeen(13, 14, 3));
-        System.out.println(hasTeen(13, 14, 15));
+        public static void main(String[] args) {
+            System.out.println(hasTeen(1, 2, 3));
+            System.out.println(hasTeen(13, 2, 3));
+            System.out.println(hasTeen(13, 14, 3));
+            System.out.println(hasTeen(13, 14, 15));
 
-        System.out.println(isTeen(1));
-        System.out.println(isTeen(17));
-        System.out.println(isTeen(22));
-    }
+            System.out.println(isTeen(1));
+            System.out.println(isTeen(17));
+            System.out.println(isTeen(22));
+        }
 }
