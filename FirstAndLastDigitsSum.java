326ebfb05a63bde0c36f808a3e37931311e7c462
Onur Elhakan <72891423+oelhakan@users.noreply.github.com>
2020-12-29 13:09:01 +0100

    Update FirstAndLastDigitsSum.java

--- FirstAndLastDigitsSum.java
+++ FirstAndLastDigitsSum.java
@@ -1,32 +1,45 @@
-public class FirstAndLastDigitsSum {
-    public static int sumFirstAndLastDigit(int number) {
-        if (number < 0) {
-            return -1;
-        } else if (number == 0) {
-            return 0;
-        }else if(number<10){
-            return number*2;
-        }
-        int lastDigit = 0;
-        int firstDigit = 0;
-        lastDigit = number % 10;
-        while (number > 99) {
-            number = number / 10;
-        }
-        if(number<=99){
-            number = number/10;
-            firstDigit = number;
-        }
-        return firstDigit + lastDigit;
-    }
+public class FirstAndLastDigitSum {
 
-    public static void main(String[] args) {
-        System.out.println(sumFirstAndLastDigit(5));
-        System.out.println(sumFirstAndLastDigit(12345));
-        System.out.println(sumFirstAndLastDigit(0));
-        System.out.println(sumFirstAndLastDigit(-123124));
+        public static int sumFirstAndLastDigit(int number) {
+            if (number < 0) {
+                return -1;
+            } else if (number == 0) {
+                return 0;
+            }else if(number<10){
+                return number*2;
+            }
+            int lastDigit = 0;
+            int firstDigit = 0;
+            lastDigit = number % 10;
+            while (number > 99) {
+                number = number / 10;
+            }
+            if(number<=99){
+                number = number/10;
+                firstDigit = number;
+            }
+            return firstDigit + lastDigit;
+        }
+        
+//        public static int sumFirstAndLastDigit(int number) {
+//            if (number < 0) {
+//                return -1;
+//            }
+//
+//            int lastDigit = number % 10;
+//
+//            while (number > 9) {
+//                number /= 10;
+//            }
+//            return lastDigit + number;
+//        }
+//    }
+
+        public static void main(String[] args) {
+            System.out.println(sumFirstAndLastDigit(5));
+            System.out.println(sumFirstAndLastDigit(12345));
+            System.out.println(sumFirstAndLastDigit(0));
+            System.out.println(sumFirstAndLastDigit(-123124));
+        }
     }
 }
-
-
-
